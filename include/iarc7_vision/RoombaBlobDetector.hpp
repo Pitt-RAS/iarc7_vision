0d350bd8a3a2c4f48ad46db29f260c0c2e9cab1d
Aaron Miller <acmiller273@gmail.com>
2018-01-17 00:25:07 +0000

    Formatting changes

--- include/iarc7_vision/RoombaBlobDetector.hpp
+++ include/iarc7_vision/RoombaBlobDetector.hpp
@@ -11,14 +11,15 @@ namespace iarc7_vision
 {
 
 class RoombaBlobDetector {
+  public:
+    void detect(const cv::cuda::GpuMat& image,
+                std::vector<cv::Rect>& boundRect);
+  private:
     void boundMask(const cv::cuda::GpuMat& mask,
                    std::vector<cv::Rect>& boundRect);
     void dilateBounds(const cv::cuda::GpuMat& image,
                       std::vector<cv::Rect>& boundRect);
     void thresholdFrame(const cv::cuda::GpuMat& image, cv::cuda::GpuMat& dst);
-  public:
-    void detect(const cv::cuda::GpuMat& image,
-                std::vector<cv::Rect>& boundRect);
 };
 
 }
