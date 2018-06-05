e1a1fbad11626617177eb6aef75789ee138e2321
Aaron Miller <acmiller273@gmail.com>
2018-06-04 23:03:04 -0400

    Reduced update rate

--- src/VisionNode.cpp
+++ src/VisionNode.cpp
@@ -249,7 +249,7 @@ int main(int argc, char **argv)
             private_nh, "message_queue_item_limit");
 
     // Loop rate
-    ros::Rate rate (300);
+    ros::Rate rate (100);
 
     // Initialize the vision classes
     ros::Time start_time = ros::Time::now();
